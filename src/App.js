28d08ea292757b5ec6aaa70d69db2e8665a03060
Hamma Azarok <hammaagazarok@gmail.com>
2022-08-11 18:34:04 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div className="page-container">
+        <Routes>
+          <Route
+            path="/"
+            element={<Books />}
+          />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
